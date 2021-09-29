11348c29960fd832d347e3715aebe169cd763fd7
Josef Schönbäck <69212054+strobosaur@users.noreply.github.com>
2021-09-29 12:08:34 +0200

    App skeleton

---
+++ src/components/SongList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SongList = () => {
+  return ( 
+    <div className="song-list">
+      <ul>
+        <li>Polygon Window</li>
+        <li>Tundra</li>
+        <li>Slider</li>
+        <li>Dark Passenger</li>
+      </ul>
+    </div>
+   );
+}
+ 
+export default SongList;
